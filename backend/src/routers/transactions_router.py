c1701fd5053fb5b16a45c36a8dabcd613c8c5c49
Itay.Talias <itay.talias@gmail.com>
2022-11-14 19:06:35 +0200

    adding routers

--- backend/src/routers/transactions_router.py
+++ backend/src/routers/transactions_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Request
-from ..db.dal_sql import CONNECTOR
+from src.db.dal_sql import CONNECTOR
 import pymysql as mysql
+from src.models.transaction_data_class import Transactions
 
 
 router = APIRouter()
@@ -9,17 +10,30 @@ router = APIRouter()
 @router.get("/transactions", status_code=200)
 def get_all_transactions():
     try:
-        transactions = CONNECTOR.get_all_transactions()
+        transactions = CONNECTOR.get_all_transactions_by_user()
     except mysql.MySQLError as e:
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e)
     return transactions
 
 
 @router.post("/transactions", status_code=201)
-async def add_transactions(request: Request):
-    pass
+async def add_transactions(new_transaction: Transactions):
+    try:
+        transaction_id = CONNECTOR.add_transaction(new_transaction)
+        new_transaction.transaction_id = transaction_id
+    except mysql.MySQLError as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e)
+    return new_transaction
 
 
 @router.delete("/transactions", status_code=204)
-def add_transactions():
-    pass
+async def add_transactions(request: Request):
+    try:
+        transaction_id = await request.json()
+        if (len(transaction_id) == 0):
+            raise ValueError("transaction_id - empty")
+        CONNECTOR.remove_transaction_by_id(transaction_id=transaction_id["id"])
+    except mysql.MySQLError as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e)
+    except mysql.ValueError as e:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=e)
