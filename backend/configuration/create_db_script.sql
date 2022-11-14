d1a4d641e2fa609caf373116ed96440d5366d8b5
Itay.Talias <itay.talias@gmail.com>
2022-11-14 19:04:52 +0200

    change db script

--- backend/configuration/create_db_script.sql
+++ backend/configuration/create_db_script.sql
@@ -5,16 +5,39 @@ USE sql_bank;
 -- -----------------------------------------------------
 -- Table `sql_bank`
 -- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Users_info(
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    balance VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS Transactions_info(
     transaction_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     amount INT NOT NULL,
     category VARCHAR(50) NOT NULL,
     vendor VARCHAR(50) NOT NULL,
-    is_depoist BOOLEAN NOT NULL
+    is_depoist BOOLEAN NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES Users_info(user_id)
 );
 
-INSERT INTO Transactions_info (amount,category,vendor,is_depoist) VALUES(10,"Transportation","bus",False);
-INSERT INTO Transactions_info (amount,category,vendor,is_depoist) VALUES(100,"Food","cat food",False);
-INSERT INTO Transactions_info (amount,category,vendor,is_depoist) VALUES(200,"Entertainment","cat toys",False);
-INSERT INTO Transactions_info (amount,category,vendor,is_depoist) VALUES(1700,"Entertainment","cat video on tiktok",True);
+CREATE TABLE IF NOT EXISTS Categories_info(
+    category VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+
+
+
+INSERT INTO Categories_info VALUES("Transportation");
+INSERT INTO Categories_info VALUES("Food");
+INSERT INTO Categories_info VALUES("Entertainment");
+INSERT INTO Users_info VALUES(1,"itay","123456",1000);
+INSERT INTO Transactions_info VALUES(1,10,"Transportation","bus",False,1);
+INSERT INTO Transactions_info  VALUES(2,100,"Food","cat food",False,1);
+INSERT INTO Transactions_info VALUES(3,200,"Entertainment","cat toys",False,1);
+INSERT INTO Transactions_info VALUES(4,1700,"Entertainment","cat video on tiktok",True,1);
+
+
+
+
