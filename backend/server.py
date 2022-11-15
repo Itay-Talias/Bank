154bc5eb26fcd8e619f0ba40316e199e0c5af77c
Itay.Talias <itay.talias@gmail.com>
2022-11-15 23:01:26 +0200

    adding conf

--- backend/server.py
+++ backend/server.py
@@ -2,10 +2,27 @@ from fastapi import FastAPI
 import uvicorn
 from src.routers import transactions_router
 from src.routers import users_roter
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 app.include_router(transactions_router.router)
 app.include_router(users_roter.router)
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
