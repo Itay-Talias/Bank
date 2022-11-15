2cecc5ed839cbba8a60b6a5518f8efb94b8dcfd6
Itay.Talias <itay.talias@gmail.com>
2022-11-15 17:03:41 +0200

    adding abstract method

--- backend/src/db/dal.py
+++ backend/src/db/dal.py
@@ -1,24 +1,30 @@
 from abc import ABC, abstractmethod
+from typing import List
 from src.models.transaction_data_class import Transactions
+from src.models.user_data_class import User
 
 
 class DAL(ABC):
     @abstractmethod
-    def get_user_by_id(self, user_id: int = 1):
+    def get_user_by_id(self, user_id: int) -> User:
         pass
 
     @abstractmethod
-    def get_all_transactions_by_user(self, user_id: int = 1):
+    def get_all_transactions_by_user(self, user_id: int) -> List[Transactions]:
         pass
 
     @abstractmethod
+    def remove_transaction_by_id(self, transaction_id: int) -> Transactions:
+        pass
+
+    @ abstractmethod
     def remove_transaction_by_id(self, transaction_id: int):
         pass
 
-    @abstractmethod
+    @ abstractmethod
     def add_transaction(self, new_transaction: Transactions, user_id: int = 1) -> int:
         pass
 
-    @abstractmethod
+    @ abstractmethod
     def change_balance(self, is_depoist: bool, amount: int, user_id: int):
         pass
