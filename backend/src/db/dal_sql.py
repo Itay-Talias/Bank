1835b37eadbe319348853d4a71e5486f2e1f254b
Itay.Talias <itay.talias@gmail.com>
2022-11-15 17:02:57 +0200

    adding singelton

--- backend/src/db/dal_sql.py
+++ backend/src/db/dal_sql.py
@@ -2,44 +2,41 @@ from src.db.dal import DAL
 import pymysql as mysql
 from src.db.queries import *
 from src.models.transaction_data_class import Transactions
-import json
+from typing import List
+from src.models.user_data_class import User
+from configuration.db_cofiguration import *
 
 
-DEFAULT_HOST = "localhost"
-DEFAULT_USER = "root"
-DEFAULT_DB = "sql_bank"
-DEFAULT_PWD = ""
-
-
-class dalSQL(DAL):
+class DalSQL(DAL):
     def __init__(self, user: str = DEFAULT_USER, pwd: str = DEFAULT_PWD, db: str = DEFAULT_DB, host: str = DEFAULT_HOST):
         self.connection = mysql.connect(
             host=host, user=user, password=pwd, db=db, charset="utf8", cursorclass=mysql.cursors.DictCursor, autocommit=True)
 
-    def get_user_by_id(self, user_id: int = 1):
+    def get_user_by_id(self, user_id: int) -> User:
         with self.connection.cursor() as cursor:
             cursor.execute(SELECT_USER_BY_ID, [user_id])
             result = cursor.fetchall()
-            return result
+            newUser = User(**result)
+            return newUser
 
-    def get_all_transactions_by_user(self, user_id: int):
+    def get_all_transactions_by_user(self, user_id: int) -> List[Transactions]:
         with self.connection.cursor() as cursor:
             cursor.execute(SELECT_TRANSACTIONS_BY_USER_ID, [user_id])
             result = cursor.fetchall()
-            return result
+            transactions = [Transactions(**t) for t in result]
+            return transactions
 
-    def get_transaction_by_id(self, transaction_id: int):
+    def get_transaction_by_id(self, transaction_id: int) -> Transactions:
         with self.connection.cursor() as cursor:
             cursor.execute(SELECT_TRANSACTION_BY_ID, [transaction_id])
             result = cursor.fetchone()
-            return result
+            return Transactions(**result)
 
     def remove_transaction_by_id(self, transaction_id: int):
         with self.connection.cursor() as cursor:
             transaction = self.get_transaction_by_id(transaction_id)
-            print(transaction)
             self.change_balance(
-                not transaction["is_depoist"], transaction["amount"], transaction["user_id"])
+                not transaction.is_depoist, transaction.amount, transaction.user_id)
             cursor.execute(DELETE_TRANSACTION, [transaction_id])
             self.connection.commit()
 
@@ -64,7 +61,14 @@ class dalSQL(DAL):
             self.connection.commit()
 
 
-try:
-    CONNECTOR = dalSQL()
-except Exception as e:
-    print(e)
+CONNECTOR = None
+
+
+def get_db_connector():
+    global CONNECTOR
+    if CONNECTOR is None:
+        try:
+            CONNECTOR = DalSQL()
+        except Exception as e:
+            print(e)
+    return CONNECTOR
