86a50294c2fab6401ffd7e418ec290e3d203ccdc
Itay.Talias <itay.talias@gmail.com>
2022-11-16 13:35:01 +0200

    adding user class

---
+++ fronted/src/Model/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(user_id, username, password, balance) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+}
+
+export default User;
